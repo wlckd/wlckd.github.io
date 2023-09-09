8a17692b073ed3556a5094972b7639fe2174a7a9
wlckd <wlckd@protonmail.com>
2023-09-09 17:15:59 -0600

    clean

--- assets/scripts/playAudio.js
+++ assets/scripts/playAudio.js
@@ -1,14 +1,9 @@
-// Get a reference to the <h1> and <audio> elements
 const h1Element = document.getElementById("playAudio");
 const audioElement = document.getElementById("audio");
 
-// Add a click event listener to the <h1> element
 h1Element.addEventListener("click", function () {
-    // Check if the audio is currently playing, and pause it if it is
     if (!audioElement.paused) {
         audioElement.pause();
     }
-    
-    // Play the audio
     audioElement.play();
 });
